1ebc6340214d6362743700fb865afe4975a1f525
Tonic78 <guymertz@gmaiL.com>
2020-07-30 11:48:54 +0200

    add filter checkbox

--- src/SearchOptions.js
+++ src/SearchOptions.js
@@ -4,7 +4,7 @@ import { Form, Col } from "react-bootstrap";
 export default function SearchOptions({ params, onParamChange }) {
   return (
     <Form className="mb-4">
-      <Form.Row>
+      <Form.Row className="align-items-end">
         <Form.Group as={Col}>
           <Form.Label>Description</Form.Label>
           <Form.Control
@@ -21,6 +21,16 @@ export default function SearchOptions({ params, onParamChange }) {
             value={params.location}
             name="location"
             type="text"
+          />
+        </Form.Group>
+        <Form.Group as={Col} xs="auto" className="ml-2">
+          <Form.Check
+            className="mb-2"
+            onChange={onParamChange}
+            value={params.full_time}
+            name="full_time"
+            label="Only Full Time"
+            type="checkbox"
           />
         </Form.Group>
       </Form.Row>
